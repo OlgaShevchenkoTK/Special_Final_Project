17425fe8d7708c662b288f623d6ca1bc209e7244
OLGA <lelya.shevchenko.2013@mail.ru>
2023-03-16 11:15:38 +0900

    Added code fore select the string array i

--- Final_project/Program.cs
+++ Final_project/Program.cs
@@ -2,4 +2,27 @@
 //массив из строк, длина которых меньше или равна 3 символа. Первоначальный массив
 //можно задать с клавиатуры, либо задать на старте выполнения алгоритма.
 
+//ПОРЯДОК ДЕЙСТВИЙ
+// Преобразовать введенный текст в массив строк
+// Выбрать значения меньше или равно 3 символа
+// Выбранные значения сохранить в новый массив строк
+// Вывести в консоль результат
 
+void SelectionText(string[] text)
+{
+    string[] resultSelectionText = new string[text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length <= 3)
+            resultSelectionText[i] = text[i];
+        else resultSelectionText[i] = " ";
+        Console.Write(resultSelectionText[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Input the text:");
+string text = Console.ReadLine();
+string[] words = text.Split(new char[] { ' ' });
+SelectionText(words);
